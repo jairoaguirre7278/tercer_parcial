3b21a62a39538a70adcd16c73ae4e73fdd749cdb
jairoaguirre7278 <Jaguirre7278@unisalamanca.edu.co>
2026-06-03 11:03:49 -0500

    agregar schemas Pydantic separados para entrada y salida_1

--- cargo_track/routers/envios.py
+++ cargo_track/routers/envios.py
@@ -2,16 +2,17 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
 from ..database import get_session
 from ..models import Envio
+from ..schemas import EnvioRead
 
 router = APIRouter(prefix="/envios", tags=["Envíos"])
 
 
-@router.get("/", response_model=list[Envio])
+@router.get("/", response_model=list[EnvioRead])
 def listar_envios(session: Session = Depends(get_session)):
     return session.exec(select(Envio)).all()
 
 
-@router.get("/{envio_id}", response_model=Envio)
+@router.get("/{envio_id}", response_model=EnvioRead)
 def obtener_envio(envio_id: int, session: Session = Depends(get_session)):
     envio = session.get(Envio, envio_id)
     if not envio:
